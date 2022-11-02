0314af5e526b71587624853def7d4eb1f83d3e57
pheng <pheng@DESKTOP-QG4GF5C>
2022-11-03 08:27:36 +0900

    20221103 [UPDATE] 문제풀이

--- JacaStudy/src/chapter5/ch5_7.java
+++ JacaStudy/src/chapter5/ch5_7.java
@@ -1,17 +1,17 @@
 package chapter5;
 
+import java.util.Scanner;
+
 public class ch5_7 {
 
 	public static void main(String[] args) {
-		
 		if(args.length != 1) {
-			System.out.println("USAGE: java Exercise5_7 3120");
+			System.out.println("USAGE: ch5_7 3120");
 			System.exit(0);
 		}
 		
 		//문자열을 숫자로 변환. 입력한 값이 숫자가 아닐 경우 예외가 발생
 		int money = Integer.parseInt(args[0]);
-		
 		System.out.println("money = " + money);
 		
 		int[] coinUnit = { 500, 100, 50, 10 }; //동전의 단위
@@ -20,16 +20,19 @@ public class ch5_7 {
 		for(int i = 0; i < coinUnit.length; i++) {
 			int coinNum = 0;
 		
-			coinNum = money / coinUnit[i];
-			 
+			coinNum = money / coinUnit[i]; //금액을 동전단위로 나눠서 필요한 동전의 개수를 구함
+			
+			if(coinNum > coin[i]) {
+				coinNum = 5;
+				coin[i] = 0;
+			} else {
+				coin[i] -= coinNum;
+			}
+			
+			money =- (coinNum*coinUnit[i]);
 		
-		
-		
-		
-		System.out.println(coinUnit[i] + "원: " + coinNum);
+			System.out.println(coinUnit[i] + "원: " + coinNum);
 		}
-		
-		
 		
 		if(money > 0) {
 			System.out.println("거스름돈이 부족합니다.");
@@ -37,8 +40,6 @@ public class ch5_7 {
 		}
 		
 		System.out.println("= 남은 동전의 개수 =");
-		
-		
 		
 		for(int i = 0; i < coinUnit.length; i++) {
 			System.out.println(coinUnit[i] + "원: " + coin[i]);
--- JacaStudy/src/chapter5/ch5_8.java
+++ JacaStudy/src/chapter5/ch5_8.java
@@ -1,5 +1,7 @@
 package chapter5;
 
+import java.util.Arrays;
+
 public class ch5_8 {
 
 	public static void main(String[] args) {
@@ -8,22 +10,22 @@ public class ch5_8 {
 		int[] counter = new int[4];
 		int count = 0;
 		
+		System.out.println(Arrays.toString(answer));
 		for(int i = 0; i < answer.length; i++) {
+			Arrays.sort(answer);
 			if(answer[i] == answer[i+1]) {
 				count += 1;
-			} else {
-				continue;
-			}
-			System.out.print(answer[i]);
+			} 
 		}
+		System.out.print(count);
 		
-		for(int i = 0; i < counter.length; i++) {
-			
+//		for(int i = 0; i < counter.length; i++) {
+//			
 //			if(cou) {
 //				System.out.print("*");
 //			}
-			
-			System.out.println();
-		}
+//			
+//			System.out.println();
+//		}
 	}
 }
